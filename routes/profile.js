0cc39ae779454f60c05fdbe44d83af7128e35010
rehanarroihan <third2014project@gmail.com>
2018-11-15 08:35:51 +0700

    creating post model

--- routes/profile.js
+++ routes/profile.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-const mongoose = require('mongoose');
 const passport = require('passport');
 
 const profileModel = require('../models/Profile');
